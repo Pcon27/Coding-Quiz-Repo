c82d21f1aceeaebbd5035019bc4ba24edd1da666
pconsag <pconsagra@g.ucla.edu>
2021-06-27 19:58:04 -0700

    add framework for the Javascript

---
+++ quizEdit1.js
@@ -0,0 +1,13 @@
+// first step is to create variables: countownTimer, timeRemaining, button, correctAnswer, incorrectAnswer1, incorrectAnswer2, incorrectAnswer3, correctAnswer Total
+// create memory of questions to be asked?
+// create a start button in HTML and link it to a java function to start the beggining of the game 
+// when the button is clicked, a timer stars, when the button is clicked, a set of questions is displayed
+// if the correct answer is clicked, hide the previous text, and show the next round of questions and responses, track of the response through a web API 
+// if the incorrect answer, subtract the timeRemaining by 10 seconds, then, hide the previous text, and show the next round of questions and responses.
+// if there are no more questions OR if the timeRemanining is zero, hide the questions and thhe timer and prompt a message saying the game is over
+//store the first score into a web API, then if the next score is greater than or equal too the previous score, prmompt a highscores page to pop up
+// the three letter prompt should allow three letters, if the total string length is greater than four, than prompt a message saying please only type four letters
+// save the score and thhe initials on the web API
+
+
+
