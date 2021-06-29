3d7e3580bdf361668234523c8ad58688baa1a957
pconsag <pconsagra@g.ucla.edu>
2021-06-28 21:17:35 -0700

    start timer at click of button, then hide timer when time =0

--- quizEdit1.js
+++ quizEdit1.js
@@ -12,12 +12,37 @@
 var quizStart
 var countdownTimer
 var timeRemaining
+var timer = document.querySelector(".timer");
+// var timeRemaining = 180;
 
 // function started by a button click
-function start() { 
+function start() {
+
+// establish time remaining
+    var timeRemaining = 5;
 
     // start countdown timer
-    countdownTimer = startTimer(function() {
+    countdownTimer = setInterval(function() {
     
-        timeRemaining = 180 
+        timeRemaining--;
+        timer.textContent = timeRemaining;
+
+    
+
+    if(timeRemaining === 0) {
+        // Stops execution of action at set interval
+        //When the countdown reaches zero, we want the action to stop. The clearInterval() method stops the execution of the interval. It takes one parameter: the variable name assigned to the interval
+        clearInterval(countdownTimer);
+        // Calls function to create and append image
+        timer.textContent = ""
+      }
+
+    }, 1000);
+}
+
+
+
+
+
+
 
